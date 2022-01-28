67651faf823ee98a96f7c95c21c48bf333805fc8
Matheus Duarte de Freitas <freickup@gmail.com>
2022-01-28 13:42:08 -0300

    Updates

---
+++ precache-manifest.3ca10dfc8e1125248b07f6a5ca6302ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9c6860c6d21dbffb7269537ca1a6e0",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "117f431193786258d0c0",
+    "url": "/trivia/static/css/main.c5e666b5.chunk.css"
+  },
+  {
+    "revision": "4e8e9df52097136acd71",
+    "url": "/trivia/static/js/2.bf804aae.chunk.js"
+  },
+  {
+    "revision": "5aa783d4f5b59d53a5afca5b85236dd5",
+    "url": "/trivia/static/js/2.bf804aae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "117f431193786258d0c0",
+    "url": "/trivia/static/js/main.845c1d76.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  }
+]);
